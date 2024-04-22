09ded8468bd68e45bb6706b7cf42833a0900d728
abdoBadawy74 <badawyabdulrahman099@gmail.com>
2024-04-22 05:24:11 +0200

    add services section

--- script.js
+++ script.js
@@ -1,10 +1,66 @@
 // funcitonality for the hamburger menu
 
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
+const hamburger = document.querySelector(".hamburger");
+const navLink = document.querySelector(".nav__link");
 
-hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
+hamburger.addEventListener("click", () => {
+  navLink.classList.toggle("hide");
 });
 
 // End hamburger menu
+
+// Start slider services
+document.addEventListener("DOMContentLoaded", function () {
+  const servicesContent = document.querySelector(".services-content");
+  const services = document.querySelectorAll(".service");
+  const bullets = document.querySelectorAll(".bult");
+
+  bullets.forEach((bullet, index) => {
+    bullet.addEventListener("click", () => {
+      // Remove selected class from all bullets and services
+      bullets.forEach((b) => b.classList.remove("selected"));
+      services.forEach((s) => s.classList.remove("selected-service"));
+
+      // Add selected class to the clicked bullet and corresponding service
+      bullet.classList.add("selected");
+      services[index].classList.add("selected-service");
+    });
+  });
+});
+
+// slider index line
+document.addEventListener("DOMContentLoaded", function() {
+    const indexElement = document.getElementById("index");
+    const lengthElement = document.getElementById("length");
+    const lineElement = document.getElementById("line");
+    const bullets = document.querySelectorAll(".bult");
+  
+    // Initial index and length values
+    let currentIndex = 0;
+    const maxLength = bullets.length;
+  
+    // Update index and length elements
+    function updateIndexAndLength() {
+      indexElement.textContent = "0"+(currentIndex + 1); // Adding 1 to start from 1 instead of 0
+      lengthElement.textContent = "0"+maxLength;
+      if (currentIndex === 0) {
+        lineElement.style.width = "10%"; // Set width to 0 if currentIndex is 0
+      } else {
+        lineElement.style.width = `${(currentIndex / maxLength) * 100}%`;
+      }
+    }
+  
+    // Click event listener for bullets
+    bullets.forEach((bullet, index) => {
+      bullet.addEventListener("click", () => {
+        currentIndex = index;
+        updateIndexAndLength();
+      });
+    });
+  
+    // Initialize index and length elements
+    updateIndexAndLength();
+  });
+  
+
+// End slider services
